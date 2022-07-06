f4be08cda3a1e0b5f55be79249993dd22191cc22
orores27 <sabah.errifi@gmail.com>
2022-07-06 15:38:31 +0200

    suite

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
-import { Card, Row, Button } from 'react-bootstrap';
+import { Card, Row, Button, Col } from 'react-bootstrap';
+import React, {useState} from "react";
 
 // props = propriétés
 const Pizza = (props) => {
-
-  return (
-        <>
-        <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={props.lapizza.image} />
-            <Card.Body>
-                <Card.Title>{props.lapizza.name}</Card.Title>
-                <Card.Text>
-                    <Row>
-                    </Row>
-                </Card.Text>
-                <Button variant='primary'>Choisir cette pizza</Button>
-            </Card.Body>
-        </Card>
-        </>
+    const[taille, setTaille] = useState('small');
+    const[quantite, setQuantite] = useState(1);
+        return (
+                <>
+                <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={props.lapizza.image} />
+                    <Card.Body>
+                        <Card.Title>{props.lapizza.name}</Card.Title>
+                        <Card.Text>
+                            <Row>
+                                <Col md={6}>
+                                    <h6>Taille:
+                                        <select
+                                            value={taille}
+                                            onChange={(e) => setTaille(e.target.value)}
+                                        >
+                                            {props.lapizza.varients.map(taille => (
+                                                <option value={taille}>{taille}</option>
+                                            ))}
+                                        </select>
+                                    </h6>
+                                </Col>
+                                <Col md={6}>
+                                    <h6>Quantité :<br/>
+                                    <select 
+                                        value={quantite}
+                                        onChange={(e) => setQuantite(e.target.value)}
+                                    >
+                                        {[...Array(10).keys()].map((v,i) =>(
+                                            <option value={i+1}>{i+1}</option>
+                                        ))}
+                                    </select>
+                                    </h6>
+                                </Col>
+                            </Row>
+                        </Card.Text>
+                        <Button variant='primary'>Choisir cette pizza</Button>
+                    </Card.Body>
+                </Card>
+                </>
     )
 };
 
